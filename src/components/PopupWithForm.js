af6a4d6576974d0cdd1ec023e0fa8ea129b761c3
Pavel Generalov <generalovpn@yandex.ru>
2023-05-02 13:57:58 +0300

    fix: Исправить замечания ревью

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,9 +4,9 @@ function PopupWithForm({name, title, isOpen, onClose, children}) {
   return (
     <div className={`popup popup_type_${name} ${isOpen ? "popup_opened" : ""}`}>
       <div className="popup__container">
-        <button className="popup__close" type="button" onClick={onClose}></button>
+        <button className="popup__close" type="button" onClick={onClose} />
         <h2 className="popup__name">{title}</h2>
-        <form className="popup__form popup__form_type_avatar" name={`form-${name}`}>
+        <form className="popup__form" name={`form-${name}`}>
           {children}
           <button type="submit" className="popup__btn-save">Сохранить</button>
         </form>
